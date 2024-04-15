8b78009fc5347bc4fc22fcab3b1ba1cb662f27e4
Alan Bergland <aob2x@virginia.edu>
2024-04-15 09:01:28 -0400

    small changes

--- gwas.nogrm.perms.R
+++ gwas.nogrm.perms.R
@@ -1,5 +1,5 @@
-
-.libPaths(c("/home/bal7cg/R/goolf/4.3")); .libPaths()
+.libPaths(c("/project/berglandlab/Rlibs_4.3.1/")); .libPaths()
+#.libPaths(c("/home/bal7cg/R/goolf/4.3")); .libPaths()
 library(RhpcBLASctl)
 blas_set_num_threads(1)
 #***************************
@@ -20,7 +20,7 @@ args = commandArgs(trailingOnly=TRUE)
 id = as.numeric(args[1])
 #id = 1
 #id = 1#use this when testing to see if script functions correctly
-numCores <- as.numeric(args[2]) 
+numCores <- as.numeric(args[2])
 #numCores = 2
 print(id)
 print(numCores)
@@ -33,12 +33,12 @@ print(INPUT)
 reftable = readRDS(paste0(INPUT,"dgrpreftable"))
 
 #load in array table
-arrayref = readRDS(paste0(INPUT, "ref.table"))
+arrayref = readRDS(paste0(INPUT, "ref.table")))
 #filter to this array's section
 arrayref = arrayref[array == id]
 print(arrayref)
 out = foreach(f = as.vector(arrayref[[1]]), .errorhandling = "remove") %do% {
-   f = 1
+  # f = 1
   print(f)
   #load in our permuted phenotype file based on array
   phenotable = readRDS(paste0(INPUT,"/phenoperms/phenopermutation",f))
@@ -46,27 +46,27 @@ out = foreach(f = as.vector(arrayref[[1]]), .errorhandling = "remove") %do% {
   melt = melt(phenotable, id.vars = ("DGRP"), variable.name = "fullpheno", value.name = "mean")
   #phenotype determined by array table
   pheno.id = as.character(arrayref[ID_var_1 == f]$ID_var_2)
-  
-  
+
+
   dt = melt[fullpheno == pheno.id]
   #merge in the values for reftable
   dt = merge(dt, reftable, by = "DGRP" )
-  dt = na.omit(dt)#remove na's 
+  dt = na.omit(dt)#remove na's
   #fix ral ids
-  dt = dt %>% 
-    dplyr::rename("ral_id" = DGRP) %>% 
+  dt = dt %>%
+    dplyr::rename("ral_id" = DGRP) %>%
     mutate(ral_id = gsub("DGRP", "line", ral_id))
   print(dim(dt))
-  
-  
-  
-  
-  
+
+
+
+
+
   #re organize
   colnames(dt)[3] <- "avg"
 
-  
-  
+
+
 
   #try making to numberic
   dt$ral_id = gsub( "line_","", dt$ral_id)
@@ -80,34 +80,30 @@ out = foreach(f = as.vector(arrayref[[1]]), .errorhandling = "remove") %do% {
   #GMMAT creates a null model that uses wolbachia status as fixed effect on phenotype, and the identity matrix as a (nonexistent) random effect. family refers to the method used to calculate the model
   #fit a GLMM to the data
   modelqtl <- glmmkin(fixed = avg ~ WolbachiaStatus_NA, data = dt, kins = grm, id = "ral_id2",family = gaussian(link = "identity"))
-  
-  
-  
+
+
+
   #run GMMAT
   #create  path for output gmmat file to be saved to- using the phenotype variable allows it to be saved to the corresponding directory
-  outputs = paste(INPUT , 
-                  "permnogrmGWAS/", 
+  outputs = paste(INPUT ,
+                  "permnogrmGWAS/",
                   f,"-",pheno.id, ".txt",
                   sep = "")
-  
+
   print(str(modelqtl))
   print(ingds)
   print(outputs)
-  
-  
+
+
   #the glmm.score function scores the impact of each snp agains the null model. minor allele frequency and missing data cutoff can be specified to reduce unwanted computation. compututation can also be sped up by specifying the number of snps calculated per batch, and how many cores to use
   glmm.score(modelqtl, infile = ingds, outfile = outputs, MAF.range = c(0.05,0.95), miss.cutoff = 0.15, ncores = numCores ,
              nperbatch = 100, verbose = T)
-  
+
   print ("done")
   # #replace file with rds file to save space
   x = fread(outputs)
-  saveRDS(x, paste(outputs,".RDS"))
+  saveRDS(x, paste(outputs,".RDS", sep=""))
   file.remove(outputs)#remove txt file
   # print("finished")
-  
+
 }
-
-
-
-
