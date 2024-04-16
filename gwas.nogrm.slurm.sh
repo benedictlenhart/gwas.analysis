15a88a8cab8e65b9055fbf220b865fe62050f784
Alan Bergland <aob2x@virginia.edu>
2024-04-16 09:57:17 -0400

    Update gwas.nogrm.slurm.sh

--- gwas.nogrm.slurm.sh
+++ gwas.nogrm.slurm.sh
@@ -7,14 +7,15 @@
 #SBATCH -n 1 # one task
 #SBATCH -t 3:30:00 #<= this may depend on your resources
 #SBATCH --mem 20G #<= this may depend on your resources
-#SBATCH --mail-type=end
-#SBATCH --mail-user=bal7cg@virginia.edu
-#SBATCH -e /scratch/bal7cg/score_error/gwas.gmmat.%A_%a.err # Standard error
-#SBATCH -o /scratch/bal7cg/score_output/gwas.gmmat.%A_%a.out # Standard output
+#SBATCH -e /scratch/aob2x/score_error/gwas.gmmat.%A_%a.err # Standard error
+#SBATCH -o /scratch/aob2x/score_output/gwas.gmmat.%A_%a.out # Standard output
 #SBATCH -p standard
 #SBATCH -A berglandlab_standard
-### sbatch  --array=1 /standard/vol186/bergland-lab/Adam/gwas/gwas.nogrm.slurm.sg
+### sbatch  --array=1 /standard/vol186/bergland-lab/Adam/gwas/gwas.analysis/gwas.nogrm.slurm.sh
+### cat /scratch/aob2x/score_error/gwas.gmmat.
 ###9760 tasks tasks
+### sacct -j 60223545
+
 
 module load gcc/11.4
 module load openmpi/4.1.4
@@ -22,7 +23,7 @@ module load R/4.3.1
 
 #define variables
 jobid=${SLURM_ARRAY_TASK_ID}
-wd="/standard/vol186/bergland-lab/Adam/gwas/gwas.analysis" #working directory
+wd="/standard/vol186/bergland-lab/Adam/gwas/gwas.analysis/" #working directory
 ncores=${SLURM_CPUS_PER_TASK}
 
 Rscript ${wd}gwas.nogrm.perms.R  \
