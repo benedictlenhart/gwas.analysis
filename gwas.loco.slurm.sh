75ca9d37f1497653b1585c1ed994e25ab78d3b33
benedictlenhart <63316724+benedictlenhart@users.noreply.github.com>
2024-05-02 13:29:37 -0400

    Add files via upload

---
+++ gwas.loco.slurm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+###goal, creat a slurm script that gathers data from permutation files
+#SBATCH -J residuals.perm # A single job name for the array
+#SBATCH -c 4
+#SBATCH -N 1 # on one node
+#SBATCH -n 1 # one task
+#SBATCH -t 3:30:00 #<= this may depend on your resources
+#SBATCH --mem 20G #<= this may depend on your resources
+#SBATCH -e /scratch/bal7cg/score_error/gwas.gmmat.%A_%a.err # Standard error
+#SBATCH -o /scratch/bal7cg/score_output/gwas.gmmat.%A_%a.out # Standard output
+#SBATCH -p largemem
+#SBATCH -A berglandlab_standard
+### sbatch  --array=1 /standard/vol186/bergland-lab/Adam/gwas/gwas.analysis/gwas.loco.slurm.sh
+### cat /scratch/aob2x/score_error/gwas.gmmat.
+###9760 tasks tasks
+### sacct -j 60223545
+
+
+module load gcc/11.4
+module load openmpi/4.1.4
+module load R/4.3.1
+
+#define variables
+jobid=${SLURM_ARRAY_TASK_ID}
+wd="/standard/vol186/bergland-lab/Adam/gwas/gwas.analysis/" #working directory
+ncores=${SLURM_CPUS_PER_TASK}
+
+Rscript ${wd}locogrm.gwas.perms.R  \
+        ${jobid} \
+        ${ncores}
---
+++ locogrm.gwas.perms.R
@@ -0,0 +1,103 @@
+.libPaths(c("/project/berglandlab/Rlibs_4.3.1/")); .libPaths()
+#.libPaths(c("/home/bal7cg/R/goolf/4.3")); .libPaths()
+library(RhpcBLASctl)
+blas_set_num_threads(1)
+#***************************
+### libraries
+library(GMMAT)
+library(data.table)
+library(tidyverse)
+library(foreach)
+library(doParallel)
+registerDoParallel(5)
+
+#library(ggplot2)
+
+print("libraries loaded")
+
+args = commandArgs(trailingOnly=TRUE)
+#create job id from array
+id = as.numeric(args[1])
+#id = 1
+#id = 1#use this when testing to see if script functions correctly
+numCores <- as.numeric(args[2])
+#numCores = 4
+print(id)
+print(numCores)
+
+# saveRDS(task.id, paste0(INPUT, "newphenotaskidchart"))
+INPUT = "/standard/vol186/bergland-lab/Adam/gwas/"#path of working directory
+
+print(INPUT)
+#load in reftable
+reftable = readRDS(paste0(INPUT,"dgrpreftable"))
+
+#load in array table
+arrayref = readRDS(paste0(INPUT, "ref.table"))
+#filter to this array's section
+arrayref = arrayref[array == id]
+print(arrayref)
+out = foreach(f = as.vector(arrayref[[1]]), .errorhandling = "remove") %do% {
+   f = 1
+  print(f)
+  #load in our permuted phenotype file based on array
+  phenotable = readRDS(paste0(INPUT,"/phenoperms/phenopermutation",f))
+  #melt phenotable
+  melt = melt(phenotable, id.vars = ("DGRP"), variable.name = "fullpheno", value.name = "mean")
+  #phenotype determined by array table
+  pheno.id = as.character(arrayref[ID_var_1 == f]$ID_var_2)
+  
+  
+  dt = melt[fullpheno == pheno.id]
+  #merge in the values for reftable
+  dt = merge(dt, reftable, by = "DGRP" )
+  dt = na.omit(dt)#remove na's
+  #fix ral ids
+  dt = dt %>%
+    dplyr::rename("ral_id" = DGRP) %>%
+    mutate(ral_id = gsub("DGRP", "line", ral_id))
+  print(dim(dt))
+  
+  
+  
+  
+  
+  #re organize
+  colnames(dt)[3] <- "avg"
+  
+  
+  
+  
+  #try making to numberic
+  dt$ral_id = gsub( "line_","", dt$ral_id)
+  dt$ral_id2 = as.numeric(dt$ral_id)#numeric removes excess zeroes
+  dt$ral_id2 = paste0("line_", dt$ral_id2)
+ 
+  #create a second loop that goes through chromosomes
+  chroms= c("2L", "2R", "3L", "3R", "X")
+  chrom.out = foreach( c = chroms) %do% {
+    #c = "2L"
+    #load in the grm for that chromosome
+    grm = readRDS(paste0(INPUT, "No", c, "GRM"))
+    #assign gds filename based on chromosome 
+    ingds = paste0(INPUT,c,".gds")
+    modelqtl <- glmmkin(fixed = avg ~ WolbachiaStatus_NA, data = dt, kins = grm, id = "ral_id2",family = gaussian(link = "identity"))
+    
+    outputs = paste(INPUT ,
+                    "permlocoGWAS/",
+                    f,"-",pheno.id,"_",c, ".txt",
+                    sep = "")
+    glmm.score(modelqtl, infile = ingds, outfile = outputs, MAF.range = c(0.05,0.95), miss.cutoff = 0.15, ncores = numCores ,
+               nperbatch = 100, verbose = T)
+    
+    print ("done")
+    # #replace file with rds file to save space
+    x = fread(outputs)
+    saveRDS(x, paste(outputs,".RDS", sep=""))
+    file.remove(outputs)#remove txt file
+    # print("finished")
+    
+  }
+ 
+  
+}
