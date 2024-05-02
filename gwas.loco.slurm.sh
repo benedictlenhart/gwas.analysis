26a6bbbbb7203974db24ecc50014cea258d747ad
benedictlenhart <63316724+benedictlenhart@users.noreply.github.com>
2024-05-02 13:31:58 -0400

    Update gwas.loco.slurm.sh

--- gwas.loco.slurm.sh
+++ gwas.loco.slurm.sh
@@ -2,14 +2,14 @@
 #
 ###goal, creat a slurm script that gathers data from permutation files
 #SBATCH -J residuals.perm # A single job name for the array
-#SBATCH -c 4
+#SBATCH -c 5
 #SBATCH -N 1 # on one node
 #SBATCH -n 1 # one task
 #SBATCH -t 3:30:00 #<= this may depend on your resources
 #SBATCH --mem 20G #<= this may depend on your resources
 #SBATCH -e /scratch/bal7cg/score_error/gwas.gmmat.%A_%a.err # Standard error
 #SBATCH -o /scratch/bal7cg/score_output/gwas.gmmat.%A_%a.out # Standard output
-#SBATCH -p largemem
+#SBATCH -p standard
 #SBATCH -A berglandlab_standard
 ### sbatch  --array=1 /standard/vol186/bergland-lab/Adam/gwas/gwas.analysis/gwas.loco.slurm.sh
 ### cat /scratch/aob2x/score_error/gwas.gmmat.
